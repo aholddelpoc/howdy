f7e87f115445780ecec7fe40fb8dead7e43181af
phukan <ujjalphukan@gmail.com>
2017-09-04 21:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,10 +192,15 @@ def makeWineWithMealFood(data):
 	cur=db.product.find( { "product_id" : { "$in": food_wine_id }})
 	speech = 'Matching Wine items for '+food_item+ ' are: '
 	for item in cur:
-		speech = speech + '\n' +  '<br\>' +  '<a >click</a>' + item['name']+" ( Price: "+item['price'] + " ) "+ '\n'
+		speech = speech + '\n' + item['name']+" ( Price: "+item['price'] + " ) "+ '\n'
 	speech = speech + '\n' + 'Please type "Add to Cart item name " to add to your Cart' + '\n'
+	{
+  		"type": "message",
+ 		 "text": "<ss type =\"wink\">;)</ss>"
+	}
 	
 	return {
+		"message": message,
 		"speech": speech,
 		"displayText": speech,
 		"source": "webhookdata"
