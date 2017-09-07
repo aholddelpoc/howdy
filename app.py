e17887c1c56b3be9de8ee3967b5d856d26205352
phukan <ujjalphukan@gmail.com>
2017-09-07 20:59:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,8 +129,12 @@ def makeWebhookResultForGetChemicalSymbol(data):
 
 def makeWebhookResultForGetWineProduct(data):
 	user_name=getUserName(data)	
-	wine_item = data.get("result").get("parameters").get("wine_product")
+	#wine_item = data.get("result").get("parameters").get("wine_product")
 	quantity = data.get("result").get("parameters").get("Quantity")
+	serial_number = data.get("result").get("parameters").get("number")
+	food_item = data.get("result").get("parameters").get("Food_Item")
+	print(food_item)
+	print(serail_number)
 	if wine_item not in wine_items:
 		wine_items.append(wine_item)
 	result = db.add_to_cart.find({"user_name":user_name,"product_name":wine_item})
